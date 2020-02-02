ed4bec919e31985174fdaa60ad591a5ad4649541
deccer <decccer@outlook.com>
2020-02-02 23:32:05 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+
 dotnet build
+mkdir -p ./DemoGame/bin/Debug/netcoreapp3.1/Mods
 cp ./ModLogger/bin/Debug/netstandard2.0/*.dll ./DemoGame/bin/Debug/netcoreapp3.1/Mods
 cp ./ModRenderer/bin/Debug/netstandard2.0/*.dll ./DemoGame/bin/Debug/netcoreapp3.1/Mods
