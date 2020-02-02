8cffb0fd68f87291c81b7c290f92d240b94a8ef1
deccer <decccer@outlook.com>
2020-02-02 22:42:59 +0100

    Smaller GameMod resolution

--- DemoGame/Program.cs
+++ DemoGame/Program.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Reflection;
 using Core;
 using DryIoc;
-using DryIoc.MefAttributedModel;
 
 namespace DemoGame
 {
     internal class Program
     {
+        private readonly ILogger _logger;
+
         public static void Main(string[] args)
         {
             var compositionRoot = CreateCompositionRoot();
@@ -24,26 +25,14 @@ namespace DemoGame
             var container = new Container();
 
             var modDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Mods");
-            var entryPoints = new List<Type>();
 
-            var entryPointAssemblyFiles = Directory.GetFiles(modDirectory, "Mod*.dll");
-
-            Console.WriteLine("Found Mods:");
-            Console.WriteLine(string.Join(Environment.NewLine, entryPointAssemblyFiles));
-
-            var entryPointAssemblies = entryPointAssemblyFiles
-                .Select(assemblyFile => Assembly.LoadFile(assemblyFile));
-
-            foreach (var entryPointAssembly in entryPointAssemblies)
-            {
-                var types = entryPointAssembly.GetTypes();
-                var entryPointTypes = types
-                    .Where(exportedType => exportedType.Name == "EntryPoint");
-                entryPoints.AddRange(entryPointTypes);
-            }
-
-            Console.WriteLine("Found EntryPoints in Mods:");
-            Console.WriteLine(string.Join(Environment.NewLine, entryPoints.Select(type => type.Assembly.GetName().Name)));
+            var entryPoints = Directory.GetFiles(modDirectory, "Mod*.dll")
+                .Select(Assembly.LoadFile)
+                .SelectMany(entryPointAssembly =>
+                {
+                    return entryPointAssembly.GetTypes()
+                        .Where(exportedType => exportedType.Name == "EntryPoint");
+                });
 
             foreach (var entryPoint in entryPoints)
             {
@@ -56,12 +45,13 @@ namespace DemoGame
 
         public Program(ILogger logger)
         {
-            logger.Write("calling Program.ctor");
+            _logger = logger;
+            _logger.Write("calling Program.ctor");
         }
 
         private void Run(string[] args)
         {
-            Console.WriteLine("Args: {0}", string.Join(";", args));
+            _logger.Write(string.Format("Args: {0}", string.Join(";", args)));
         }
     }
 }
