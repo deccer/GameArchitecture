40e67b48704991d85cb42c8772cb18fd927ed06e
deccer <decccer@outlook.com>
2020-02-03 19:45:06 +0100

    Add primitive messagebus

--- Demo/Game.cs
+++ Demo/Game.cs
@@ -9,9 +9,14 @@ namespace Demo
 {
     internal class Game : IGame
     {
-        protected Game(ILogger logger)
-            : base(logger)
+        private readonly MessageBus _messageBus;
+
+        private readonly ILogger _logger;
+
+        public Game(ILogger logger, MessageBus messageBus)
         {
+            _logger = logger;
+            _messageBus = messageBus;
         }
 
         private void Cleanup()
