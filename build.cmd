bef51b4c13ded00bdce1b4d9d41c5c93912d7dd8
Mihai Stan <stanmihai17cs@gmail.com>
2020-02-14 00:22:09 +0100

    Add build file for CMD

---
+++ build.cmd
@@ -0,0 +1,6 @@
+
+dotnet build
+mkdir .\Demo\bin\Debug\netcoreapp3.1\Mods
+copy .\ModWindow\bin\Debug\netstandard2.0\*.dll .\Demo\bin\Debug\netcoreapp3.1\Mods
+copy .\ModLogger\bin\Debug\netstandard2.0\*.dll .\Demo\bin\Debug\netcoreapp3.1\Mods
+copy .\ModRenderer\bin\Debug\netstandard2.0\*.dll .\Demo\bin\Debug\netcoreapp3.1\Mods
